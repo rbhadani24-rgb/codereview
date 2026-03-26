e7e066ca189c206dd98ca7b974452ac31c53cece
rbhadani24-rgb <rbhadani24@gmail.com>
2026-03-27 00:34:24 +0530

    Refactor codereview.py for improved readability

--- codereview.py
+++ codereview.py
@@ -8,7 +8,7 @@ from fastapi.middleware.cors import CORSMiddleware
 #os.environ["OPENAI_API_KEY"] = "OPENAI_API_KEY"
 
 client = OpenAI()
-
+print("KEY LOADED:", os.getenv("OPENAI_API_KEY"))
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
